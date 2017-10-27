fa518ab7e6a067a514d2c62df1679876b8e425ec
Will Badr <walebadr@amazon.com>
2017-10-27 18:06:58 +1300

    added quotes

--- code-deploy.sh
+++ code-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 killall python
 cd /home/ec2-user
-export BACKEND_API=http://modul-LoadB-19X7K43CY3SUI-1487929617.us-east-1.elb.amazonaws.com
+export BACKEND_API="http://modul-LoadB-19X7K43CY3SUI-1487929617.us-east-1.elb.amazonaws.com"
 pip install -r requirements.txt
 nohup python app.py &>/dev/null &
